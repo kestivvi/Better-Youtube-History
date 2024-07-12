d7e7f40f6b9fc097a424404c85b63d3873577a21
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2024-07-12 16:36:50 +0200

    Database refactor

--- src/shared/calendar/fetchSupabaseForCalendarId.ts
+++ src/shared/calendar/fetchSupabaseForCalendarId.ts
@@ -1,6 +1,6 @@
 import { SupabaseClient } from '@supabase/supabase-js'
 
 export async function fetchSupabaseForCalendarId(supabase: SupabaseClient): Promise<string | null> {
-  const queryResponse = await supabase.from('user_calendar_ids').select('calendar_id')
-  return queryResponse.data?.[0]?.calendar_id ?? null
+  const queryResponse = await supabase.from('user_config').select('calendarId')
+  return queryResponse.data?.[0]?.calendarId ?? null
 }
--- src/shared/state/calendarId.ts
+++ src/shared/state/calendarId.ts
@@ -1,8 +1,11 @@
 import { fetchSupabaseForCalendarId } from '../calendar/fetchSupabaseForCalendarId'
+import { sessionSignal } from './auth/session'
 import { createSignal } from './createSignal'
 import { supabaseSignal } from './supabase'
 
-export const { calendarIdSignal } = createSignal('calendarId', null as string | null, {
+const KEY = 'calendarId'
+
+export const { calendarIdSignal } = createSignal(KEY, null as string | null, {
   // Initialize and check if the user has a calendar ID stored in the supabase database
   callbackAfterInitFromStorage: async (value) => {
     const calendarId = await fetchSupabaseForCalendarId(supabaseSignal.value)
@@ -14,36 +17,24 @@ export const { calendarIdSignal } = createSignal('calendarId', null as string | 
 })
 
 // Update the calendar ID in the supabase database whenever the signal's value changes
-calendarIdSignal.subscribe((value) => {
-  if (value === null) {
-    supabaseSignal.value.from('user_calendar_ids').delete().match({ calendar_id: value })
-  } else {
-    supabaseSignal.value.from('user_calendar_ids').upsert([{ calendar_id: value }])
-  }
+calendarIdSignal.subscribe(async (value) => {
+  const userId = sessionSignal.value?.user?.id
+  if (!userId) return console.error('[calendarIdSignal] User is not logged in')
+
+  supabaseSignal.value
+    .from('user_config')
+    .update([{ [KEY]: value }])
+    .eq('user_id', userId)
 })
 
 supabaseSignal.value
-  .channel('calendarChanged')
-  .on(
-    'postgres_changes',
-    { event: '*', schema: 'public', table: 'user_calendar_ids' },
-    (payload) => {
-      console.debug('[supabaseCalendarChanged] Change received!', payload)
+  .channel('userConfigChanged')
+  .on('postgres_changes', { event: '*', schema: 'public', table: 'user_config' }, (payload) => {
+    if (!(KEY in payload.new)) return
+    if (payload.new[KEY] === calendarIdSignal.value) return
 
-      if (payload.eventType === 'DELETE') {
-        // I don't know if this is necessary, or do signals memoize by default
-        if (calendarIdSignal.value !== null) {
-          calendarIdSignal.value = null
-        }
-      } else if (payload.eventType === 'INSERT') {
-        if (calendarIdSignal.value !== payload.new['calendar_id']) {
-          calendarIdSignal.value = payload.new['calendar_id']
-        }
-      } else if (payload.eventType === 'UPDATE') {
-        if (calendarIdSignal.value !== payload.new['calendar_id']) {
-          calendarIdSignal.value = payload.new['calendar_id']
-        }
-      }
-    },
-  )
+    if (calendarIdSignal.value !== payload.new[KEY]) {
+      calendarIdSignal.value = payload.new[KEY]
+    }
+  })
   .subscribe()
