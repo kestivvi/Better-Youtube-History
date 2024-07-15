a376902cd449de44e42bf109edf94549e203b18d
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2024-07-15 09:42:36 +0200

    Improve header navigation

--- src/popup/views/Header/ViewSwitchButton.tsx
+++ src/popup/views/Header/ViewSwitchButton.tsx
@@ -1,9 +1,10 @@
-import { Dispatch, SetStateAction } from 'react'
-import { View } from '../../Popup'
-import { Button, Group, Text } from '@mantine/core'
+import { Dispatch, ReactNode, SetStateAction } from 'react'
+import { Button, Group, Text, useMantineTheme } from '@mantine/core'
 import { IconSettings } from '@tabler/icons-react'
 import { IconChevronLeft } from '@tabler/icons-react'
 import { IconCode } from '@tabler/icons-react'
+
+import { View } from '../../Popup'
 
 type Props = {
   view: View
@@ -14,53 +15,48 @@ type Props = {
 const isDev = process.env.NODE_ENV == 'development'
 
 export default function ({ view, setView }: Props) {
+  const theme = useMantineTheme()
+
+  const NavigationButton = ({
+    viewToSet,
+    Icon,
+    children,
+  }: {
+    viewToSet: View
+    Icon: ReactNode
+    children: ReactNode
+  }) => (
+    <Button
+      onClick={() => setView(viewToSet)}
+      variant="transparent"
+      component="span"
+      size="compact-xs"
+      p={0}
+      style={{ color: theme.colors.gray[3] }}
+    >
+      {Icon}
+      <Text ml={5}>{children}</Text>
+    </Button>
+  )
+
   if (view !== 'HOME') {
     return (
-      <Button
-        onClick={() => setView('HOME')}
-        variant="transparent"
-        component="span"
-        size="compact-xs"
-        p={0}
-        style={{
-          color: 'white',
-        }}
-      >
-        <IconChevronLeft />
-        <Text ml={5}>Back</Text>
-      </Button>
+      <NavigationButton viewToSet={'HOME'} Icon={<IconChevronLeft />}>
+        Back
+      </NavigationButton>
     )
   }
 
   return (
     <Group>
-      <Button
-        onClick={() => setView('SETTINGS')}
-        variant="transparent"
-        component="span"
-        size="compact-xs"
-        p={0}
-        style={{
-          color: 'white',
-        }}
-      >
-        <IconSettings />
-        <Text ml={5}>Settings</Text>
-      </Button>
+      <NavigationButton viewToSet={'SETTINGS'} Icon={<IconSettings />}>
+        Settings
+      </NavigationButton>
+
       {isDev && (
-        <Button
-          onClick={() => setView('DEV')}
-          variant="transparent"
-          component="span"
-          size="compact-xs"
-          p={0}
-          style={{
-            color: 'white',
-          }}
-        >
-          <IconCode />
-          <Text ml={5}>Dev</Text>
-        </Button>
+        <NavigationButton viewToSet={'DEV'} Icon={<IconCode />}>
+          Dev
+        </NavigationButton>
       )}
     </Group>
   )
