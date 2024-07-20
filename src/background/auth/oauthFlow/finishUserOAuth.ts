b446a03ef9e398fd8980feb47fd6a73545eea4c8
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2024-07-20 14:09:54 +0200

    Refactor finishUserOAuth

--- src/background/auth/oauthFlow/finishUserOAuth.ts
+++ src/background/auth/oauthFlow/finishUserOAuth.ts
@@ -12,71 +12,71 @@ import { supabaseSignal } from "@/shared/state/supabase"
  * Used in background script as message listener on url change.
  */
 export async function finishUserOAuth(url: string) {
-  try {
-    console.debug("[finishUserOAuth] handling user OAuth callback. Url: ", url)
+  console.debug("[finishUserOAuth] handling user OAuth callback. Url: ", url)
 
-    // extract tokens from hash
-    const hashMap = parseUrlHash(url)
-    const access_token = hashMap.get("access_token")
-    const refresh_token = hashMap.get("refresh_token")
+  // Extract tokens from hash
+  const { access_token, refresh_token, provider_token, provider_refresh_token } =
+    parseUrlHash(url)
 
-    // get provider_token and provider_refresh_token
-    const provider_token = hashMap.get("provider_token")
-    const provider_refresh_token = hashMap.get("provider_refresh_token")
+  console.debug("[finishUserOAuth] access_token", access_token)
+  console.debug("[finishUserOAuth] refresh_token", refresh_token)
+  console.debug("[finishUserOAuth] provider_token", provider_token)
+  console.debug("[finishUserOAuth] provider_refresh_token", provider_refresh_token)
 
-    console.debug("[finishUserOAuth] access_token", access_token)
-    console.debug("[finishUserOAuth] refresh_token", refresh_token)
-    console.debug("[finishUserOAuth] provider_token", provider_token)
-    console.debug("[finishUserOAuth] provider_refresh_token", provider_refresh_token)
-
-    if (!access_token || !refresh_token || !provider_token || !provider_refresh_token) {
-      throw new Error("no tokens found in URL hash")
-    }
-
-    // check if they work
-    const { data, error } = await supabaseSignal.value.auth.setSession({
-      access_token,
-      refresh_token,
-    })
-
-    if (error) throw error
-
-    // set the session
-    sessionSignal.value = data.session as SessionType
-    providerTokenSignal.value = provider_token
-    providerRefreshTokenSignal.value = provider_refresh_token
-
-    console.debug("[finishUserOAuth] session", data.session)
-
-    // finally redirect to a post oauth page
-    const redirectPage = chrome.runtime.getURL("redirectPage.html")
-    chrome.tabs.update({ url: redirectPage })
-
-    // fetch and save provider token info
-    await fetchAndSaveTokenInfo(provider_token)
-
-    // fetch calendar id
-    const calendarId = await fetchSupabaseForCalendarId(supabaseSignal.value)
-    calendarIdSignal.value = calendarId
-
-    sessionStateSignal.value = "LOGGED_IN"
-    console.debug("[finishUserOAuth] finished handling user OAuth callback")
-  } catch (error) {
-    console.error(error)
+  if (!access_token || !refresh_token || !provider_token || !provider_refresh_token) {
+    console.error("no tokens found in URL hash")
+    return
   }
+
+  // Set the supabase session
+  const { data, error } = await supabaseSignal.value.auth.setSession({
+    access_token,
+    refresh_token,
+  })
+
+  if (error) {
+    console.error("[finishUserOAuth] error setting session", error)
+    return
+  }
+
+  console.debug("[finishUserOAuth] session", data.session)
+
+  // Set signals
+  sessionSignal.value = data.session as SessionType
+  providerTokenSignal.value = provider_token
+  providerRefreshTokenSignal.value = provider_refresh_token
+
+  // finally redirect to a post oauth page
+  const redirectPage = chrome.runtime.getURL("redirectPage.html")
+  chrome.tabs.update({ url: redirectPage })
+
+  // fetch and save provider token info
+  await fetchAndSaveTokenInfo(provider_token)
+
+  // fetch calendar id
+  const calendarId = await fetchSupabaseForCalendarId(supabaseSignal.value)
+  calendarIdSignal.value = calendarId
+
+  sessionStateSignal.value = "LOGGED_IN"
+  console.debug("[finishUserOAuth] finished handling user OAuth callback")
 }
 
 /**
  * Helper method used to parse the hash of a redirect URL.
  */
 function parseUrlHash(url: string) {
-  const hashParts = new URL(url).hash.slice(1).split("&")
-  const hashMap = new Map(
-    hashParts.map((part) => {
-      const [name, value] = part.split("=")
-      return [name, value]
-    }),
-  )
+  return new URL(url).hash
+    .slice(1)
+    .split("&")
+    .reduce(
+      (acc, part) => {
+        const [key, value] = part.split("=")
 
-  return hashMap
+        if (!key || !value) return acc
+
+        acc[key] = value
+        return acc
+      },
+      {} as Record<string, string>,
+    )
 }
